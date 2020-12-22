e72afbed3b42db8f6d6c6e6e2cf3e716c00d1d75
Jan Hrček <honza.hrk@gmail.com>
2020-12-22 19:48:23 +0100

    Extract usages

--- src/DeclDeps.hs
+++ src/DeclDeps.hs
@@ -1,62 +1,86 @@
 {-# LANGUAGE DerivingVia       #-}
 {-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
 {-| Discover dependencies between declarations.
 We say that Declaration X depends on Y if Y is used anywhere in the declaration of X.
 -}
 module DeclDeps (dumpTopLevelDefinitions) where
 
+import Data.Containers.ListUtils (nubOrd)
 import Data.Foldable (traverse_)
 import Data.Function ((&))
+import Data.List (sortOn)
 import qualified Data.Map.Strict as Map
 import Data.Maybe (mapMaybe)
 import qualified Data.Set as Set
 import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 import DynFlags (DynFlags)
 import HieBin (hie_file_result, readHieFile)
-import HieTypes (HieAST (..), HieASTs (getAsts), HieFile (..), NodeInfo (..), TypeIndex)
+import HieTypes (ContextInfo (Use), HieAST (..), HieASTs (getAsts), HieFile (..), NodeInfo (..),
+                 TypeIndex, identInfo)
 import HieUtils (flattenAst)
-import Module (moduleName, moduleNameString, moduleUnitId, unitIdString)
-import Name (nameModule_maybe)
+import Module (Module, moduleName, moduleNameString, moduleUnitId, unitIdString)
+import Name (Name, nameModule_maybe, nameOccName, occNameString)
 import NameCache (NameCache, initNameCache)
-import Outputable (ppr, showSDoc)
 import UniqSupply (mkSplitUniqSupply)
 
 
 dumpTopLevelDefinitions :: DynFlags -> FilePath -> IO ()
-dumpTopLevelDefinitions dynFlags hieFilePath = withHieFile hieFilePath $ \HieFile {{-hie_module, - current module-} hie_asts} -> do
+dumpTopLevelDefinitions _ hieFilePath = withHieFile hieFilePath $ \HieFile {hie_module, hie_asts} -> do
     let topLevelDeclAsts :: [HieAST TypeIndex]
         topLevelDeclAsts = getAsts hie_asts
             & Map.elems
             & concatMap flattenAst
-            & filter
-                (\Node {nodeInfo} ->
-                    -- Assumption: which  HieAST Node constitutes a top level definition can only be determined from nodeAnnotations
-                    let anns = nodeAnnotations nodeInfo
-                    in (Set.member ("AbsBinds","HsBindLR") anns && Set.member ("FunBind","HsBindLR") anns && Set.member ("Match","Match") anns)
-                    || Set.member ("DataDecl","TyClDecl") anns
-                    || Set.member ("DataDecl","TyClDecl") anns
-                )
+            & filter isTopLevelDecl
     topLevelDeclAsts
-        & mapMaybe (\Node {nodeSpan, nodeChildren} ->
-            -- Assumption: The name of the top-level definition can be extracted from the first child of the definition node
-            case nodeChildren of
-                [] -> Nothing
-                (firstChild:_) -> case firstChild of
-                    Node {nodeInfo} -> case Map.toList (nodeIdentifiers nodeInfo) of
-                        [] -> Nothing
-                        ((Left _modName,_identDetails):_) -> Nothing
-                        ((Right name,_identDetails):_) -> fmap
-                            (\mn -> -- could we just use hie_module from above instead of doing this fmap?
-                                ( nodeSpan
-                                , Outputable.showSDoc dynFlags (ppr name)
-                                , moduleNameString (moduleName mn)
-                                , unitIdString (moduleUnitId mn)
-                                )
-                            )
-                            (nameModule_maybe name)
-            )
-        & traverse_ print
+        & mapMaybe (\hieAst -> fmap (,getUsedSymbols hieAst) (getTopLevelDeclInfo hie_module hieAst))
+        & traverse_ (\(declSym, usedSyms) -> do
+            Text.putStrLn (prettySymbol declSym)
+            traverse_ (\s -> Text.putStrLn $ "    " <> prettySymbol s) usedSyms
+        )
+
+
+isTopLevelDecl :: HieAST a -> Bool
+isTopLevelDecl Node {nodeInfo} =
+    -- Assumption: which  HieAST Node constitutes a top level definition can only be determined from nodeAnnotations
+    let anns = nodeAnnotations nodeInfo
+    in (Set.member ("AbsBinds","HsBindLR") anns && Set.member ("FunBind","HsBindLR") anns && Set.member ("Match","Match") anns)
+    || Set.member ("DataDecl","TyClDecl") anns
+    || Set.member ("DataDecl","TyClDecl") anns
+
+
+getTopLevelDeclInfo :: Module -> HieAST a -> Maybe Symbol
+getTopLevelDeclInfo currentModule Node{{-nodeSpan-} nodeChildren} =
+    case nodeChildren of
+        [] -> Nothing
+        -- Assumption: The name of the top-level definition can be extracted from the first child of the definition node
+        (firstChild:_) -> case firstChild of
+            -- Assumption: it's ok to just look at the first identifier (if any)
+            Node {nodeInfo} -> case Map.lookupMin (nodeIdentifiers nodeInfo) of
+                Just (Right name {- Lefts contain ModuleName (e.g. in imports) -}, _) -> Just $ mkSymbol name currentModule
+                _ -> Nothing
+
+
+getUsedSymbols :: HieAST a -> [Symbol]
+getUsedSymbols =
+    sortOn _symbolName
+    . nubOrd
+    . mapMaybe (\Node{nodeInfo} -> case Map.lookupMin (nodeIdentifiers nodeInfo) of
+        Just (Right name, identDetails)
+            | Set.member Use (identInfo identDetails) ->
+                fmap (mkSymbol name) (nameModule_maybe name)
+        _ -> Nothing)
+    . flattenAst
+
+
+mkSymbol :: Name -> Module -> Symbol
+mkSymbol  name modul = Symbol
+    (PackageName . Text.pack . unitIdString $ moduleUnitId modul)
+    (ModuleName . Text.pack . moduleNameString $ moduleName modul)
+    (SymbolName . Text.pack . occNameString $ nameOccName name)
 
 withHieFile :: FilePath -> (HieFile -> IO a) -> IO a
 withHieFile hieFilePath act = do
@@ -70,10 +94,17 @@ mkNameCache = do
     uniq_supply <- mkSplitUniqSupply 'z'
     return $ initNameCache uniq_supply []
 
-data Decl = Decl
-  { _declPackage  :: PackageName,
-    _declModule   :: ModuleName,
-    _declFunction :: FunctionName
+prettySymbol :: Symbol -> Text
+prettySymbol (Symbol p m s) =
+    -- TODO use some formatting lib
+    unSymbolName s <> Text.replicate (25 - Text.length (unSymbolName s)) " "
+    <> unModuleName m <> " ("
+    <> unPackageName p <> ")"
+
+data Symbol = Symbol
+  { _symbolPackage :: PackageName,
+    _symbolModule  :: ModuleName,
+    _symbolName    :: SymbolName
   }
   deriving stock (Show, Eq, Ord)
 
@@ -81,6 +112,6 @@ newtype PackageName = PackageName {unPackageName :: Text} deriving (Eq, Ord, Sho
 
 newtype ModuleName = ModuleName {unModuleName :: Text} deriving (Eq, Ord, Show) via Text
 
-newtype FunctionName = FunctionName {unFunctionName :: Text} deriving (Eq, Ord, Show) via Text
+newtype SymbolName = SymbolName {unSymbolName :: Text} deriving (Eq, Ord, Show) via Text
 
-type Edge = (Decl, Decl)
+type Edge = (Symbol, Symbol)
